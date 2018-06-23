14d760079ba84c0b078f1ef7bec9f55fa944dbca
DzikaKuna400 <stanik.lukasz@outlook.com>
2018-06-23 23:40:43 +0200

    Documentation...

--- html/search/all_1.js
+++ html/search/all_1.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['gauss_5fcoarr',['gauss_coarr',['../namespacecoarray__lib.html#a1dbf310aeabc215161c4368152ea4e37',1,'coarray_lib']]],
   ['gauss_5fseq',['gauss_seq',['../namespacesequiential__lib.html#a4e5b7e29fdd4105f584465535bfdeb50',1,'sequiential_lib']]]
 ];
